c48b7f8b1e0bea62447f7f0bd4660689888de2d2
arohanajit <arohanajit232@gmail.com>
2020-05-27 08:14:35 +0530

    commit

---
+++ data_load.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b7a519d52251c75bebc1e7215c3fe5103a2333d2ec0f602bf513329ec7fd3c1d
+size 4840
---
+++ models.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e2db9cafb15419f92fee13b3069cfe1a3bbfa3bb69581b06821964358737a709
+size 1880
